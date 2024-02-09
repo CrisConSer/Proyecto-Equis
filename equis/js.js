fbc4747b20b18ca0797a3ca474cdb56eb6a04f1d
CrisConSer <cristinacondeserrano@gmail.com>
2024-02-09 17:33:16 +0100

    update

--- equis/js.js
+++ equis/js.js
@@ -13,3 +13,28 @@ window.addEventListener("scroll", function() {
 
   lastScrollTop = currentScrollTop;
 });
+
+// Accion dots
+
+let dot1 = document.getElementById("dot-one");
+let dot2 = document.getElementById("dot-two");
+let dot3 = document.getElementById("dot-three");
+let dot4 = document.getElementById("dot-four");
+let imgchange = document.getElementById("imagen-principal");
+
+dot1.onclick = function () {
+  imgchange.src = "./imagen/Carrusel/dot1.jpg";
+}
+
+dot2.onclick = function () {
+  imgchange.src = "./imagen/Carrusel/dot2.jpg";
+}
+
+dot3.onclick = function () {
+  imgchange.src = "./imagen/Carrusel/dot3.jpg";
+}
+
+dot4.onclick = function () {
+  imgchange.src = "./imagen/Carrusel/dot4.jpg";
+}
+
